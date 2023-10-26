0070187ceb93e0f341bc8a807de795c957b5d5e7
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-10-26 10:33:28 -0300

    feat: update link

--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -1,21 +1,21 @@
-import { GitIcon } from "../Icons/git-icon";
-import { InIcon } from "../Icons/in-icon";
-import { InstaIcon } from "../Icons/insta-icon";
+import { GitIcon } from '../Icons/git-icon';
+import { InIcon } from '../Icons/in-icon';
+import { InstaIcon } from '../Icons/insta-icon';
 
-import"./social-btns.scss";
+import './social-btns.scss';
 
-export function SocialBtns(){
-  return(
-      <div className="contact">
-        <a href="https://www.linkedin.com/in/gabrielneves-dev/" target="blank">
-          <InIcon/>
-        </a>
-        <a href="https://github.com/GapNeves" target="blank">
-          <GitIcon/>
-        </a>
-        <a href="https://www.instagram.com/_pereiiraa_/" target="blank">
-          <InstaIcon/>
-        </a>
-      </div>
-  )
+export function SocialBtns() {
+  return (
+    <div className="contact">
+      <a href="https://www.linkedin.com/in/gabrielneves-dev/" target="blank">
+        <InIcon />
+      </a>
+      <a href="https://github.com/GapNeves" target="blank">
+        <GitIcon />
+      </a>
+      <a href="https://www.instagram.com/gabriel.pn_/" target="blank">
+        <InstaIcon />
+      </a>
+    </div>
+  );
 }

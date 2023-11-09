adc0e3ee7d47f5e3a390dd6afcde367f5de17066
Gabriel Pereira <gabriel.neves84@hotmail.com>
2023-11-09 12:58:35 -0300

    feat: Adding footer

---
+++ src/app/components/footer/footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './footer.scss'
+
+export default function footer() {
+  return (
+    <div className='footer'>
+      <h3>
+      Copyright © 2023
+        <span className='devs'>
+          devsgabriel
+        </span>
+      todos os direitos reservados.
+      </h3>
+    </div>
+  )
+}
